ccc2773b77981458a491298c5a7e4d31629985b8
Dhrumil Mehta <dhrumil.mehta@columbia.edu>
2024-02-03 13:31:29 -0500

    fix button and styles for button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function myFunction() {
     var x = document.getElementById("more-stories");
-    if (x.style.display === "none") {
+
+
+    // check if x.style.display doesn't exist
+    if (x.style.display === "none" || !x.style.display) {
       x.style.display = "flex";
+      console.log("x.style.display is none");
     } else {
       x.style.display = "none";
+      console.log("x.style.display is not none");
+
     }
   }
